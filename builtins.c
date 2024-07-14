1ca24569b04cc301dd496583a9c0d048120f479f
Denochikoe <denochikoe@gmail.com>
2024-07-14 14:05:08 +0300

    Shell

--- builtins.c
+++ builtins.c
@@ -19,7 +19,7 @@ void (*check_for_builtins(vars_t *vars))(vars_t *vars)
 	for (i = 0; check[i].f != NULL; i++)
 	{
 		if (_strcmpr(vars->av[0], check[i].name) == 0)
-				break;
+			break;
 	}
 	if (check[i].f != NULL)
 		check[i].f(vars);
