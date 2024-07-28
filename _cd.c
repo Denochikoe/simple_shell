1cc59ef82a38b5d4beaf6c70a0430379008f2b55
Denochikoe <denochikoe@gmail.com>
2024-07-29 00:34:36 +0300

    cd implementation

--- _cd.c
+++ _cd.c
@@ -23,43 +23,51 @@ void update_pwd(void)
  */
 void _cd(vars_t *vars)
 {
-	char *dir;
-	char *prev_dir;
-	char *cwd;
+	char *dir, *home;
+	char *oldpwd;
+	struct stat st;
 
-	if (vars->av[1] && strcmp(vars->av[1], "-") == 0)
+	if (vars->av[1] == NULL)
 	{
-		prev_dir = getenv("OLDPWD");
-		if (prev_dir == NULL)
+		home = getenv("HOME");
+		if (home == NULL)
 		{
-			print_error(vars, ": OLDPWD not set\n");
-			vars->status = 2;
+			print_error(vars, ": HOME not set\n");
+			vars->status = 1;
 			return;
 		}
-		dir = prev_dir;
+		dir = home;
+	}
+	else if (_strcmpr(vars->av[1], "-") == 0)
+	{
+		oldpwd = getenv("OLDPWD");
+		if (oldpwd == NULL)
+		{
+			print_error(vars, ": OLDPWD not set\n");
+			vars->status = 1;
+			return;
+		}
+		dir = oldpwd;
+		_puts(dir);
+		_puts("\n");
 	}
 	else
 	{
-		if (vars->av[1] == NULL)
-		{
-			dir = getenv("HOME");
-		}
-		else
-		{
-			dir = vars->av[1];
-		}
+		dir = vars->av[1];
+	}
+	if (stat(dir, &st) != 0)
+	{
+		print_error(vars, ": No such file or directory\n");
+		vars->status = 1;
+		return;
 	}
 	if (chdir(dir) != 0)
 	{
 		perror("cd");
-		vars->status = 2;
+		vars->status = 1;
 		return;
 	}
-	cwd = getenv("PWD");
-	if (cwd != NULL)
-	{
-		setenv("OLDPWD", cwd, 1);
-	}
-	update_pwd();
+	setenv("OLDPWD", getenv("PWD"), 1);
+	setenv("PWD", dir, 1);
 	vars->status = 0;
 }
